486dc0347e938357cb74b7835b13c4300bf957b7
Mykhaylo Tymofyeyev <mykhaylo.tymofyeyev@deseip.com>
2023-07-02 23:05:20 +0200

    removed unused code

--- app/page.tsx
+++ app/page.tsx
@@ -6,60 +6,36 @@ import Gradient from "../components/Gradient";
 
 import ThemeSwitcher from "../components/ThemeSwitcher";
 
-type project = {
-  _id: string;
-  title: string;
-  description: string;
-  coverImage: string;
-  imgWidth: number;
-  imgHeight: number;
-  tags: string[];
-};
+// type project = {
+//   _id: string;
+//   title: string;
+//   description: string;
+//   coverImage: string;
+//   imgWidth: number;
+//   imgHeight: number;
+//   tags: string[];
+// };
 
 export default async function Home() {
-  const projects: project[] = await getProjects();
+  // const projects: project[] = await getProjects();
   return (
     <main className="main">
       <div>
         <ThemeSwitcher />
       </div>
-      <div className="background">
-        {projects.map((project: project) => {
-          return (
-            <div
-              key={project._id}
-              id={project._id}
-            >
-              <h1>{project.title}</h1>
-              <p>{project.description}</p>
-              <p>{project.tags.join(", ")}</p>
-              <Image
-                src={project.coverImage}
-                alt={project.title}
-                width={project.imgWidth}
-                height={project.imgHeight}
-                priority
-              />
-            </div>
-          );
-        })}
-      </div>
-      <div className="gradient-box">
-        <Gradient />
-      </div>
     </main>
   );
 }
 
-const client = createClient({
-  projectId: "5lwk0a0s",
-  dataset: "production",
-  useCdn: true,
-});
+// const client = createClient({
+//   projectId: "5lwk0a0s",
+//   dataset: "production",
+//   useCdn: true,
+// });
 
-async function getProjects() {
-  const projects = await client.fetch(
-    `*[_type == "project"]{..., "coverImage": coverImage.asset->url, "imgWidth": coverImage.asset->metadata.dimensions.width, "imgHeight": coverImage.asset->metadata.dimensions.height}`
-  );
-  return projects;
-}
+// async function getProjects() {
+//   const projects = await client.fetch(
+//     `*[_type == "project"]{..., "coverImage": coverImage.asset->url, "imgWidth": coverImage.asset->metadata.dimensions.width, "imgHeight": coverImage.asset->metadata.dimensions.height}`
+//   );
+//   return projects;
+// }
