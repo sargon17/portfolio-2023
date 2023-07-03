9596f4e65c16f9cd9424ff49765b3d3c3b31363d
Mykhaylo Tymofyeyev <mykhaylo.tymofyeyev@deseip.com>
2023-07-03 10:40:37 +0200

    working on buttons

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,15 +1,32 @@
 import React from "react";
 
 import ThemeSwitcher from "./ThemeSwitcher";
+import Button from "./Button";
+
 export default function Sidebar() {
   return (
     <div className="sidebar">
       <nav className="sidebar__nav">
-        <button className="sidebar__button">hero</button>
-        <button className="sidebar__button">hero</button>
-        <button className="sidebar__button">hero</button>
-        <button className="sidebar__button">hero</button>
-        <button className="sidebar__button">hero</button>
+        <Button
+          text="hero"
+          className="sidebar__nav__button"
+        />
+        <Button
+          text="skils"
+          className="sidebar__nav__button"
+        />
+        <Button
+          text="services"
+          className="sidebar__nav__button"
+        />
+        <Button
+          text="projects"
+          className="sidebar__nav__button"
+        />
+        <Button
+          text="contacts"
+          className="sidebar__nav__button"
+        />
         <ThemeSwitcher />
       </nav>
       <div className="sidebar__title">
