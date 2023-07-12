cd8ea5469c1b6771c356cba8e655ca1122de55da
Mykhaylo <63978197+sargon17@users.noreply.github.com>
2023-07-12 20:59:41 +0200

    hided theme switcher

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,33 +1,18 @@
 import React from "react";
 
-import ThemeSwitcher from "./ThemeSwitcher";
+// import ThemeSwitcher from "./ThemeSwitcher";
 import Button from "./Button";
 
 export default function Sidebar() {
   return (
     <div className="sidebar">
       <nav className="sidebar__nav">
-        <Button
-          text="hero"
-          className="sidebar__nav__button"
-        />
-        <Button
-          text="skils"
-          className="sidebar__nav__button"
-        />
-        <Button
-          text="services"
-          className="sidebar__nav__button"
-        />
-        <Button
-          text="projects"
-          className="sidebar__nav__button"
-        />
-        <Button
-          text="contacts"
-          className="sidebar__nav__button"
-        />
-        <ThemeSwitcher />
+        <Button text="hero" className="sidebar__nav__button" />
+        <Button text="skils" className="sidebar__nav__button" />
+        <Button text="services" className="sidebar__nav__button" />
+        <Button text="projects" className="sidebar__nav__button" />
+        <Button text="contacts" className="sidebar__nav__button" />
+        {/* <ThemeSwitcher /> */}
       </nav>
       <div className="sidebar__title">
         <h1>Mykhaylo Tymofyeyev</h1>
