9ac3a0250c185b4013c7056c758fcaefa2cc7750
Mykhaylo Tymofyeyev <mykhaylo.tymofyeyev@deseip.com>
2023-07-15 14:51:30 +0200

    Hero layout working on

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,13 +1,38 @@
 import React from "react";
 
+import Gradient from "./Gradient";
+
 export default function Hero() {
   return (
     <div className="hero">
-      <div className="hero__title">
-        <h3 className="hero__title__name">Mykhaylo Tymofyeyev</h3>
-        <h2 className="hero__title__title">
-          I do <span>web sites</span>
-        </h2>
+      <div className="hero__heading">
+        <div className="hero__title">
+          <h2 className="hero__title__title">
+            Unleashing Digital{" "}
+            <span className="row-2">
+              <span className="accent">Visions</span>
+              <span className="description">Crafting Bold and Mesmerizing Websites</span>
+            </span>
+          </h2>
+        </div>
+        <div className="hero__location">
+          <p>
+            <span className="open">(</span>
+            <span className="content">milan, italy</span>
+            <span className="close">)</span>
+          </p>
+        </div>
+      </div>
+      <div className="hero__content">
+        <div className="hero__content__image">
+          <img
+            src="https://unsplash.it/1200/800"
+            alt="hero"
+          />
+        </div>
+        <div className="hero__gradient">
+          <Gradient />
+        </div>
       </div>
     </div>
   );
