3a985384c928c7362ec66dfb30ac094b30006fba
Mykhaylo Tymofyeyev <mykhaylo.tymofyeyev@deseip.com>
2023-07-16 21:44:44 +0200

    anchor developed

--- components/Slide.tsx
+++ components/Slide.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 
-export default function Slide({ children }: { children: React.ReactNode }) {
-  return <div className="slide">{children}</div>;
+export default function Slide({ children, id }: { children: React.ReactNode; id?: string }) {
+  return (
+    <div className="slide">
+      <div
+        className="anchor"
+        id={id}
+      ></div>
+      {children}
+    </div>
+  );
 }
