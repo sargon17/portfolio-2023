6de9c47bc8aa2fdb922d5aac598d258b88d4c7ff
sargon17 <mykhaylo.tymofyeyev@gmail.com>
2023-07-29 19:26:16 +0200

    delete the old layout

--- components/_Projects.tsx
+++ components/_Projects.tsx
@@ -3,23 +3,11 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { client } from "@/sanity/lib/client";
 
-import ListItem from "@/components/ListItem";
-import ListShowcase from "@/components/ListShowcase";
-
 // types
 import projectType from "@/types/project";
 
 export default function Projects() {
   const [projects, setProjects] = useState<projectType[]>([]);
-  const [currentProject, setCurrentProject] = useState<projectType | null>(projects[0] || null);
-
-  useEffect(() => {
-    getProjects();
-  }, []);
-
-  useEffect(() => {
-    console.log(projects);
-  }, [projects]);
 
   const getProjects = async () => {
     const projects = await client.fetch(`*[_type == "project"]{
@@ -37,37 +25,19 @@ export default function Projects() {
     setProjects(projects);
   };
 
-  const handleItemHover = (id: string) => {
-    const project = projects.find((project) => project._id === id);
-    if (!project) return;
-    setCurrentProject(project);
-  };
-
-  const renderProjectsList = () => {
-    return projects.map((project, index) => {
-      return (
-        <ListItem
-          id={project._id}
-          key={project._id}
-          onHover={handleItemHover}
-          isActive={currentProject?._id === project._id}
-        >
-          {project.title}
-        </ListItem>
-      );
-    });
-  };
-
   return (
     <div className="projects-page">
-      <div className="projects__container">
-        <div className="projects__container__content">
-          <ListShowcase
-            projects={projects}
-            currentProject={currentProject}
-          />
-        </div>
-        <div className="projects__container__list">{renderProjectsList()}</div>
+      <div className="navigation">
+        {projects.map((project) => {
+          return (
+            <div
+              key={"nav" + project._id}
+              className="navigation__item"
+            >
+              {project.title}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
