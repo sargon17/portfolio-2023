7f9130360c61cd4d5efb8809903cbea17f01bb34
sargon17 <mykhaylo.tymofyeyev@gmail.com>
2023-08-05 18:39:14 +0200

    setting dimension on mouse helper

--- components/MouseHelper.tsx
+++ components/MouseHelper.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { use } from "react";
 
 import { useSelector } from "react-redux";
 import clsx from "clsx";
@@ -7,12 +7,18 @@ import { useLenis } from "@studio-freight/react-lenis";
 import { useLayoutEffect, useState, useRef } from "react";
 import { RootState } from "@/contexts/mouseStore";
 
+import { motion } from "framer-motion";
+
 export default function MouseHelper() {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
   const [scrollPosition, setScrollPosition] = useState(0);
+  const [mouseDimensions, setMouseDimensions] = useState({ width: 0, height: 0 });
+  const [content, setContent] = useState("");
 
-  const content = useSelector((state: RootState) => state.content.content);
+  const newContent = useSelector((state: RootState) => state.content.content);
   const mousePositionState = useSelector((state: RootState) => state.position.position);
+
+  const mouseDimensionState = useSelector((state: RootState) => state.dimension.dimension);
 
   const lenis = useLenis(handleScroll);
 
@@ -28,6 +34,38 @@ export default function MouseHelper() {
       setMousePosition(mousePositionState);
     }
   }, [mousePositionState]);
+
+  useLayoutEffect(() => {
+    if (newContent !== content) {
+      setContent(newContent);
+    }
+  }, [newContent]);
+
+  useLayoutEffect(() => {
+    if (mouseDimensionState) {
+      setMouseDimensions(mouseDimensionState);
+    }
+  }, [mouseDimensionState]);
+
+  useLayoutEffect(() => {
+    if (mouse.current) {
+      mouse.current.animate(
+        [
+          {
+            width: `${mouseDimensions.width}px`,
+            height: `${mouseDimensions.height}px`,
+          },
+        ],
+        {
+          duration: 400,
+          fill: "forwards",
+          delay: 10,
+          // bouncy easing
+          easing: "cubic-bezier(0.175, 0.885, 0.32, 1.175)",
+        }
+      );
+    }
+  }, [mouseDimensions]);
 
   useLayoutEffect(() => {
     if (mouse.current) {
@@ -55,7 +93,13 @@ export default function MouseHelper() {
       ref={mouse}
     >
       <div className="mouse-helper__content">
-        <p>{content}</p>
+        <motion.p
+          key={content}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, transition: { duration: 0.5, delay: 0.2 } }}
+        >
+          {content}
+        </motion.p>
       </div>
     </div>
   );
