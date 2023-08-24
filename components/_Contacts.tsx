117a477e8716aa18b3f9c856f28849328edf6482
Mykhaylo <63978197+sargon17@users.noreply.github.com>
2023-08-24 14:43:57 +0300

    better gradient noise

--- components/_Contacts.tsx
+++ components/_Contacts.tsx
@@ -62,12 +62,41 @@ export default function Contacts() {
           fill="none"
           preserveAspectRatio="xMidYMin slice"
         >
-          <g filter="url(#filter0_f_462_57)">
+          <g filter="url(#blurFilter)" className="bg__color__figure">
             <path d="M906.722 1235.55C724.535 805.75 224.648 335.01 208.292 1010.41C552.814 818.157 567.589 203.495 1023.88 605.865C1031.42 418.316 888.853 230.626 2377.49 159.256C1936.24 420.688 2053.51 2019.51 2085.87 2301.01C1951.86 1446.12 1698.38 2259.82 1409.33 1069.24C1344.95 1071.34 1252.14 1276.64 1104.68 1356.67C1033.36 1395.38 1008.24 1452.16 906.722 1435.55Z" />
           </g>
+          <defs>
+            {/* blur filter */}
+            <filter id="blurFilter">
+              <feGaussianBlur stdDeviation="80" />
+            </filter>
+          </defs>
+        </svg>
+        <div className="noise"></div>
+      </div>
+      <div className="bg__noise">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1550 1055"
+          id="bg__noise__svg"
+        >
+          <rect
+            width="1550"
+            height="1055"
+            fill="url(#paint0_linear)"
+            filter="url(#noiseFilter)"
+            className="bg__noise__rect"
+          />
+          <filter id="noiseFilter">
+            <feTurbulence
+              type="fractalNoise"
+              baseFrequency="0.65"
+              numOctaves="10"
+              stitchTiles="stitch"
+            />
+          </filter>
         </svg>
       </div>
-      <div className="bg__noise"></div>
       <main>
         <div className="contacts__container">
           <h3 className="contacts__container__title">
