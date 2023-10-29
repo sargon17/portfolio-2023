6c90c0994ed379c0e111a92f1d94e28bfb0b0c2b
sargon17 <mykhaylo.tymofyeyev@gmail.com>
2023-10-29 18:41:13 +0100

    tying to add p5js

--- components/Gradient.tsx
+++ components/Gradient.tsx
@@ -1,64 +1,64 @@
 "use client";
 import React from "react";
 
+import { MutableRefObject, useEffect, useRef } from "react";
+import p5 from "p5";
+
 type GradientParams = {
   className?: string;
-  styles?: object;
-  width?: number;
-  height?: number;
 };
 
-export default function Gradient({ className, styles }: GradientParams) {
+export default function Gradient({ className }: GradientParams) {
   const baseClass = "gradient";
   const gradientClass = className ? `${baseClass} ${className}` : baseClass;
-  const gradientStyles = styles ? styles : {};
 
   return (
-    <div
-      className={gradientClass}
-      style={gradientStyles}
-    >
-      <svg
-        width="2000"
-        height="2000"
-        viewBox={`0 0 2000 2000`}
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        filter="url(#svg)"
-        preserveAspectRatio="xMidYMin slice"
-      >
-        <g clipPath="url(#clip0_102_321)">
-          <rect
-            width="2000"
-            height="2000"
-            className="gradient-bg"
-          />
-          <g filter="url(#filter0_f_102_321)">
-            <ellipse
-              className="gradient-1"
-              cx="829"
-              cy="1000"
-              rx="1629"
-              ry="600"
-            />
-            <ellipse
-              className="gradient-2"
-              cx="145"
-              cy="10"
-              rx="845"
-              ry="1000"
-            />
-            <ellipse
-              className="gradient-3"
-              cx="645"
-              cy="0"
-              rx="645"
-              ry="700"
-            />
-          </g>
-        </g>
-      </svg>
-      <div className="noise"></div>
+    <div className={gradientClass}>
+      <P5 />
+      <div className="noise">P5</div>
     </div>
   );
+}
+
+function P5() {
+  const canvasRef = useRef() as MutableRefObject<HTMLDivElement>;
+
+  useEffect(() => {
+    const sketch = (p: p5) => {
+      let canvas: p5.Renderer;
+
+      p.setup = () => {
+        canvas = p.createCanvas(canvasRef.current.clientWidth, canvasRef.current.clientHeight);
+        canvas.parent(canvasRef.current);
+        p.noLoop();
+      };
+
+      p.draw = () => {
+        p.background(255);
+        p.stroke(0);
+        p.strokeWeight(2);
+        p.noFill();
+
+        for (let i = 0; i < 100; i++) {
+          p.beginShape();
+          for (let j = 0; j < 100; j++) {
+            p.vertex(p.random(p.width), p.random(p.height));
+          }
+          p.endShape(p.CLOSE);
+        }
+      };
+
+      p.windowResized = () => {
+        p.resizeCanvas(canvasRef.current.clientWidth, canvasRef.current.clientHeight);
+      };
+    };
+
+    const p5Instance = new p5(sketch);
+
+    return () => {
+      p5Instance.remove();
+    };
+  }, []);
+
+  return <div ref={canvasRef}></div>;
 }
--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  reactStrictMode: false,
 };
 
 module.exports = nextConfig;
