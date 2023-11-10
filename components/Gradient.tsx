9920fd9429470d99e7b2e32ccd74b63241031312
sargon17 <mykhaylo.tymofyeyev@gmail.com>
2023-11-10 21:06:45 +0100

    Remove unnecessary code and comments related to SVG filters and backdrops

--- components/Gradient.tsx
+++ components/Gradient.tsx
@@ -18,47 +18,6 @@ export default function Gradient({ className, styles }: GradientParams) {
       className={gradientClass}
       style={gradientStyles}
     >
-      {/* <svg
-        width="2000"
-        height="2000"
-        viewBox={`0 0 2000 2000`}
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        filter="url(#svg)"
-        preserveAspectRatio="xMidYMin slice"
-      >
-        <g clipPath="url(#clip0_102_321)">
-          <rect
-            width="2000"
-            height="2000"
-            className="gradient-bg"
-          />
-          <g filter="url(#filter0_f_102_321)">
-            <ellipse
-              className="gradient-1"
-              cx="829"
-              cy="1000"
-              rx="1629"
-              ry="600"
-            />
-            <ellipse
-              className="gradient-2"
-              cx="145"
-              cy="10"
-              rx="845"
-              ry="1000"
-            />
-            <ellipse
-              className="gradient-3"
-              cx="645"
-              cy="0"
-              rx="645"
-              ry="700"
-            />
-          </g>
-        </g>
-      </svg> */}
-
       <div className="gradient-1"></div>
       <div className="gradient-2"></div>
       <div className="gradient-3"></div>
