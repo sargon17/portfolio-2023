ca437a171a22e119470005386aa7ea723f229ab3
sargon17 <mykhaylo.tymofyeyev@gmail.com>
2024-01-02 09:55:28 +0100

    Update package.json and server.js

--- server.js
+++ server.js
@@ -1,49 +1,41 @@
-const express = require("express");
+const { createServer } = require("http");
+const { parse } = require("url");
 const next = require("next");
-const cors = require("cors");
-const bodyParser = require("body-parser");
+const winston = require("winston");
 
 const dev = process.env.NODE_ENV !== "production";
 const hostname = "localhost";
 const port = 3000;
+// when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
-// const serviziRouter = require("./server/routes/servizi");
-// const miscRouter = require("./server/routes/misc");
-// const checkoutRouter = require("./server/routes/create-payment-intent");
-// const postDb = require("./server/routes/post-db");
-// const getDb = require("./server/routes/get-db");
-// const contact = require("./server/routes/contact");
-// const satispayRouter = require("./server/routes/satispay");
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+  transports: [new winston.transports.File({ filename: "server.log" })],
+});
 
 app.prepare().then(() => {
-  const server = express();
+  createServer(async (req, res) => {
+    try {
+      // Be sure to pass `true` as the second argument to `url.parse`.
+      // This tells it to parse the query portion of the URL.
+      const parsedUrl = parse(req.url, true);
+      const { pathname, query } = parsedUrl;
 
-  server.use(bodyParser.json());
-  var corsOptions = {
-    origin: false,
-  };
-  server.use(cors(corsOptions));
-
-  // server.use("/api/database/post", postDb);
-  // server.use("/api/checkout", checkoutRouter);
-  // server.use("/api/servizi", serviziRouter);
-  // server.use("/api/intermerda/get", getDb);
-  // server.use("/api/satispay", satispayRouter);
-  // server.use("/misc", miscRouter);
-  // server.use("/mailer", contact);
-
-  server.get("*", (req, res) => {
-    return handle(req, res);
-  });
-
-  server.use(bodyParser.urlencoded({ extended: true }));
-  server.post("/api/auth/*", (req, res) => {
-    return handle(req, res);
-  });
-
-  server.listen(3000, (err) => {
-    if (err) throw err;
-    console.log("Ready on http://localhost:3000");
-  });
+      await handle(req, res, parsedUrl);
+    } catch (err) {
+      logger.error("Error occurred handling", req.url, err);
+      res.statusCode = 500;
+      res.end("internal server error");
+    }
+  })
+    .once("error", (err) => {
+      console.error(err);
+      process.exit(1);
+    })
+    .listen(port, () => {
+      console.log(`> Ready on http://${hostname}:${port}`);
+    });
 });
