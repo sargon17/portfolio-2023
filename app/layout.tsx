74abbf97bed96ac31269589e85cef283f51def34
sargon17 <mykhaylo.tymofyeyev@gmail.com>
2024-06-12 17:27:25 +0200

    removed unused code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,9 @@
-import Head from "next/head";
-import Script from "next/script";
 import "./../styles/global.scss";
 import "./globals.css";
 import clsx from "clsx";
 
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
-
-import Providers from "./Providers";
 
 import { Trirong } from "next/font/google";
 import { Libre_Franklin } from "next/font/google";
@@ -38,28 +34,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <SpeedInsights />
       <Analytics />
-      <head>
-        <Script
-          id="matomo-analytics"
-          dangerouslySetInnerHTML={{
-            __html: `var _paq = (window._paq = window._paq || []);
-/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-_paq.push(["trackPageView"]);
-_paq.push(["enableLinkTracking"]);
-(function () {
-  var u = "//analytics.kaylo.it/";
-  _paq.push(["setTrackerUrl", u + "matomo.php"]);
-  _paq.push(["setSiteId", "1"]);
-  var d = document,
-    g = d.createElement("script"),
-    s = d.getElementsByTagName("script")[0];
-  g.async = true;
-  g.src = u + "matomo.js";
-  s.parentNode.insertBefore(g, s);
-})();`,
-          }}
-        />
-      </head>
       <body className={clsx(body.className, accent.className)}>{children}</body>
     </html>
     // </Providers>
